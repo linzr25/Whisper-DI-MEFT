9ea7d50c651d7f5793a7eb8e416c63788eb2a2b6
BlessedZ <114573415+linzr25@users.noreply.github.com>
2025-03-01 15:40:53 +0800

    Update args_mezo.py

--- mezo_trainer/args_mezo.py
+++ mezo_trainer/args_mezo.py
@@ -3,19 +3,16 @@ from transformers import TrainingArguments
 
 @dataclass
 class TrainingArguments_Mezo(TrainingArguments):
-    # 添加第一个自定义参数
     trainer: str = field(
         default=None,
         metadata={"help": "Mezo Trainer"}
     )
     
-    # 添加第二个自定义参数
     zo_eps: float = field(
         default=1e-3,
         metadata={"help": "zo_eps"}
     )
 
-    # 添加第3个自定义参数
     non_diff: bool = field(
         default=False,
         metadata={"help": "Forward Auto-Differentiation"}
